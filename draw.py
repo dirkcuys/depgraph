fd09e769812292475e6b8b8a6e729d6c4f080e80
Dirk Uys <dirkcuys@gmail.com>
2010-11-11 23:31:49 +0200

    basic SVG drawinggit add draw.py

--- draw.py
+++ draw.py
@@ -2,68 +2,18 @@
 
 import pygame, os, sys
 from pygame.locals import *
-
+import cairo
 
 class App:
 	def __init__(self, packages):
 		self.surface = None
 		self.packages = packages
 
-	def onInit(self):
-		pygame.init()
-		self.window = pygame.display.set_mode((800,600))
-		self.surface = pygame.display.get_surface()
-		
-		xoffsets = dict()
-		positions = dict()
-		
-		for package in self.packages.itervalues():
-			level = package.calcLevel()
-			xoff = 1
-			if level in xoffsets:
-				xoffsets[level] += 1
-				xoff = xoffsets[level]
-			else:
-				xoffsets[level] = 1
-				
-			positions[package] = (100*xoff, 100*level)
-			pygame.draw.circle( self.surface, (255,255,255,1), (100*xoff, 100*level), 10, 1)
-			
-		for package, pos in positions.iteritems():
-			for dep in package.dependancies:
-				pygame.draw.line( self.surface, (255,255,255,1), pos, positions[dep])
-			
-		#pygame.draw.circle( self.surface, (255,255,255,1), (100, 100), 10, 1)
-
-	def onMouseMotion(self, mouse_event):
-		#print 'Mouse!!'
-		#print mouse_event.pos
-		#pygame.draw.circle( self.surface, (255,255,255,1), mouse_event.pos, 10, 1)
-		pass
-
-	def onExit(self):
-		sys.exit(0)
-
-	def onEvent(self,events):
-		for event in events:
-			if event.type == QUIT:
-				self.onExit()
-			elif event.type == MOUSEMOTION:
-				self.onMouseMotion(event)
-			else:
-				print event
-
-	def mainLoop(self):
-		while True:
-			# only look for and get events we are interested in
-			if pygame.event.peek((MOUSEMOTION, QUIT)):
-				self.onEvent(pygame.event.get((MOUSEMOTION, QUIT)))
-			pygame.display.flip()
-			
 class Package:
 	def __init__(self, name):
 		self.name = name
 		self.dependancies = set()
+		self.position = (0.0, 0.0)
 		
 	def calcLevel(self):
 		max = 0
@@ -72,9 +22,67 @@ class Package:
 			if depLevel > max:
 				max = depLevel
 		return max + 1
+		
+	def __str__(self):
+		return '{0} {1}'.format(self.name, self.position)
+		
+		
+class SimpleSVG:
+	def __init__(self, filename):
+		self.surface = cairo.SVGSurface(filename, 1920, 1080)
+		self.context = cairo.Context(self.surface)
+		
+	def drawRect(self, x, y, w, h ):
+		self.context.set_source_rgb(0.0, 0.0, 0.0)
+		self.context.set_line_width(10)
+		self.context.rectangle(x, y, w, h)
+		self.context.stroke()
+		
+		
+	def drawText(self, x, y, text):
+		self.context.set_source_rgb(0.0, 0.0, 0.0)
+		self.context.set_line_width(0.5)
+		self.context.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+		self.context.set_font_size(10)
+		x_bearing, y_bearing, width, height = self.context.text_extents(text)[:4]
+		self.context.move_to(x - x_bearing, y - height / 2 - y_bearing)
+		self.context.text_path(text)
+		self.context.fill()
+		
+	def drawLine(self, x1, y1, x2, y2):
+		self.context.set_source_rgb(0.0, 0.0, 0.0)
+		self.context.set_line_width(1)
+		self.context.move_to(x1, y1)
+		self.context.line_to(x2, y2)
+		self.context.stroke()
+		
+	def finish(self):
+		self.surface.write_to_png('test.png')
+		self.surface.finish()
+		
+def drawGraph(packages):
+	
+	svg = SimpleSVG('output.svg')
+	xoffsets = dict()
+	for package in packages.itervalues():
+		level = package.calcLevel()
+		xoff = 1
+		if level in xoffsets:
+			xoffsets[level] += 1
+			xoff = xoffsets[level]
+		else:
+			xoffsets[level] = 1
+			
+		package.position = (100*xoff, 100*level)
+		svg.drawText(*package.position, text=package.name)
+		
+	for package in packages.itervalues():
+		for dep in package.dependancies:
+			x1, y1 = package.position
+			x2, y2 = dep.position
+			svg.drawLine( x1, y1, x2, y2)
 
 plist = dict()
-
 plist['cairo'] = Package('cairo')
 plist['libX'] = Package('libX')
 plist['qt-core'] = Package('qt-core')
@@ -99,7 +107,7 @@ plist['gimp'].dependancies.add(plist['cairo'])
 plist['libxml'].dependancies.add(plist['readline'])
 
 for package in plist.itervalues():
+	print(package)
 	print('{0} is level {1}'.format(package.name, package.calcLevel()))
-game = App(plist)
-game.onInit()
-game.mainLoop()
+	
+drawGraph(plist)
